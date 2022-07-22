397f9388707e2c14f2b069ac2fa59b2dc3a6382b
Kate <katerina.gorshkova10@gmail.com>
2022-07-22 22:07:48 +0400

    [feat]: app completed

---
+++ pages/404.js
@@ -0,0 +1,30 @@
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import Head from "next/head";
+import Heading from "../components/Heading";
+import styles from "../styles/404.module.scss";
+import React from "react";
+
+const Error = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    setTimeout(() => {
+      router.push('/');
+    }, 3000);
+  }, [router]);
+
+  return (
+    <div className={styles.wrapper}>
+      <Head>
+        <title>Error</title>
+      </Head>
+      <div>
+        <Heading text="404" tag='h1' />
+        <Heading tag="h2" text="Something is going wrong..." />
+      </div>
+    </div>
+  )
+};
+
+export default Error;
